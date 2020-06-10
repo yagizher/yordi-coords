a4bfc8dcd36c8d9619b1f57e507188541829f5cb
yordi-a <yordi@outlook.com.tr>
2020-06-10 23:42:27 +0300

    by Yordi
    by Yordi

---
+++ html/index.js
@@ -0,0 +1,54 @@
+$(function () {
+    function display(bool) {
+    if (bool) {
+        $("body").show();
+        $("#yordi").show();
+    } else {
+        $("body").hide();
+        $("#yordi").hide();
+    }
+}
+
+display(false)
+
+window.addEventListener('message', function(event) {
+
+        var item = event.data;
+
+        if (item.type === "ui") {
+            if (item.status == true) {
+                display(true)
+            } else {
+                display(false)
+            }
+        }
+
+        yordiCoords(event.data);
+
+    })
+})
+
+function yordiClose(){
+    $.post('http://yordi-coords/yordicloseButton');
+}
+
+function yordiCopy(){
+    const copyText = document.getElementById("yordiCoords").textContent;
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy");
+}
+
+function yordiCoords(data) {
+
+	if (data.type === 'yordi') {
+        yordicoordsStart(data);
+    }
+
+}
+
+function yordicoordsStart(data){
+    document.querySelector("#yordiCoords").textContent = data.text;
+}
---
+++ yordi-client.lua
@@ -0,0 +1,43 @@
+local display = false
+
+RegisterCommand("kordinat", function()
+    local playerCoords = GetEntityCoords(PlayerPedId())
+    local x = string.sub(playerCoords.x, 0, 8) 
+    local y = string.sub(playerCoords.y, 0, 8)
+    local z = string.sub(playerCoords.z, 0, 6)
+    yordiCoords('yordi', 'x = ' .. x .. ', y = ' .. y .. ', z = ' .. z)
+    SetDisplay(not display)
+end)
+
+function yordiCoords(type, text)
+	SendNUIMessage({
+        type = type,
+		text = text
+	})
+end
+
+function SetDisplay(bool)
+    display = bool
+    SetNuiFocus(bool, bool)
+    SendNUIMessage({
+        type = "ui",
+        status = bool,
+    })
+end
+
+RegisterNUICallback("yordicloseButton", function(data)
+    SetNuiFocus(false, false)
+    SetDisplay(false)
+end)
+
+Citizen.CreateThread(function()
+    while display do
+        Citizen.Wait(0)
+        DisableControlAction(0, 1, display)
+        DisableControlAction(0, 2, display)
+        DisableControlAction(0, 142, display)
+        DisableControlAction(0, 18, display)
+        DisableControlAction(0, 322, display)
+        DisableControlAction(0, 106, display)
+    end
+end)
